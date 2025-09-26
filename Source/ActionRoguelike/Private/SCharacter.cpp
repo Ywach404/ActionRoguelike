c56a8051c776d734004fb94d3e21e7ca21f45e86
Anthony Cervantez <fear.edu@proton.me>
2025-09-26 13:17:42 -0500

    Added the pointers

--- Source/ActionRoguelike/Private/SCharacter.cpp
+++ Source/ActionRoguelike/Private/SCharacter.cpp
@@ -9,6 +9,9 @@ ASCharacter::ASCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>("Spring Arm Component");
+
+	CameraComp = CreateDefaultSubobject<UCameraComponent>("CameraComponent");
 }
 
 // Called when the game starts or when spawned
